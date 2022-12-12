060f48bc737c7f4ae98bedd36efdcec60ebf4ba7
SbyreXII <113555511+SbyreXII@users.noreply.github.com>
2022-12-12 14:30:29 +0000

    Add files via upload

---
+++ MegEvent/MegEvent/MegEventApp.swift
@@ -0,0 +1,17 @@
+//
+//  MegEventApp.swift
+//  MegEvent
+//
+//  Created by user231590 on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MegEventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
