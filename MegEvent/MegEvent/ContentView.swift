17380ed553240ad5a9789ec3d3f37695ace618e3
SbyreXII <113555511+SbyreXII@users.noreply.github.com>
2022-12-12 15:44:35 +0000

    Début Tableau
    Mise en place de l'ébauche du tableau calendrier avec button qui va plutard ammener à une page de détail de l'activité

--- MegEvent/MegEvent/ContentView.swift
+++ MegEvent/MegEvent/ContentView.swift
@@ -8,12 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var activity = ["Sporting Kansas City", "Real Salt Lake", "Colorado Rapids", "Minnesota United", "+1 ekip"]
+        
+        var start = ["2", "2", "2", "2", "2"]
+        
+        var teamWins = ["2", "1", "0", "1", "1"]
+        var teamDraws = ["0", "1", "1", "1", "1"]
+        var teamLosses = ["0", "0", "1", "0", "1"]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("MegEvent' Schedule")
+            
+            HStack(spacing: 10) {
+                Text("Activity")
+                    .padding(.leading)
+                    .frame(width: 175.0, alignment: .leading)
+                Spacer()
+                Text("Start")
+                Divider()
+                    .frame(height: 20.0)
+                    .padding(.trailing)
+            }
+            .font(.subheadline)
+            .foregroundColor(.gray)
+            List(activity.indices) { i in
+                //Text("\(i+1)")
+                //Image("second")
+                HStack(spacing: 10) {
+                    Text("\(self.activity[i])")
+                        .frame(width: 200, alignment: .leading)
+                    Spacer()
+                        .frame(width: 25.0)
+                    Text("\(self.start[i])")
+                        .frame(width: 12) // replace 12 with any value for the exact result you're expection
+                    }
+                Button("Activity's details"){
+                
+            }               }
+            
         }
         .padding()
     }
